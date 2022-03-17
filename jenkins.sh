1fa67e211332b640b0693f71cf97b088e4cc8829
munisekharmandyam <96892597+Munisekhar-Devops@users.noreply.github.com>
2022-03-17 23:42:02 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ echo "--------------------"
 cd /opt
 wget -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install -y
+yum install jenkins -y
 systemctl start jenkins.service
 systemctl status jenkins.service
 #service jenkins status
